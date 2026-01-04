82a86ded2dbbc6bfdad0a76e6131d7b30d097c5b
jimtete <48908714+jimtete@users.noreply.github.com>
2026-01-04 15:33:03 +0200

    Dimitris | Implemented and used MeetingService, Used StreamRendering

--- OlympusVMS/OlympusVMS/Components/Pages/Home/Home.razor.cs
+++ OlympusVMS/OlympusVMS/Components/Pages/Home/Home.razor.cs
@@ -1,13 +1,39 @@
-using OlympusVMS.Utils.Models;
+using Microsoft.AspNetCore.Components;
+using OlympusVMS.Services.Interfaces;
+using OlympusVMS.Utils.DTOs.Meeting;
 
 namespace OlympusVMS.Components.Pages.Home;
 
-public partial class Home
+public partial class Home : ComponentBase, IDisposable
 {
-    protected string TodayText => DateTime.Today.ToShortDateString();
+    [Inject] public IMeetingService MeetingService { get; set; } = default;
+    
+    private readonly CancellationTokenSource _cts = new();
+    
 
-    protected List<GuestRecord> GuestRecords { get; } = new()
+    private static string TodayText => DateTime.Today.ToShortDateString();
+    private static bool IsLoading { get; set; } = true;
+    private static string ErrorMessage { get; set; }
+    private static IReadOnlyList<LoadTodayMeetingDto> Meetings { get; set; } = Array.Empty<LoadTodayMeetingDto>();
+
+
+    protected override async Task OnInitializedAsync()
     {
+        IsLoading = true;
         
-    };
+        Meetings = await MeetingService.LoadTodayMeetings(_cts.Token);
+
+        if (Meetings is null || Meetings.Count == 0)
+        {
+            ErrorMessage = $"{nameof(Meetings)} is empty";
+        }
+        
+        IsLoading = false;
+    }
+
+    public void Dispose()
+    {
+        _cts.Cancel();
+        _cts.Dispose();
+    }
 }
