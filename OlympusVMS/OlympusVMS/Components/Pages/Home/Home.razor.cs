9a721167bdc503b7124be1693c85890e1e37beaa
jimtete <48908714+jimtete@users.noreply.github.com>
2026-01-04 13:56:15 +0200

    Dimitris | Creation of new project OlympusVMS.Services

--- OlympusVMS/OlympusVMS/Components/Pages/Home/Home.razor.cs
+++ OlympusVMS/OlympusVMS/Components/Pages/Home/Home.razor.cs
@@ -1,6 +1,24 @@
+using OlympusVMS.Utils.Models;
+
 namespace OlympusVMS.Components.Pages.Home;
 
-public class Home_razor
+public partial class Home
 {
-    
+    protected string TodayText => DateTime.Today.ToShortDateString();
+
+    protected List<GuestRecord> GuestRecords { get; } = new()
+    {
+        new GuestRecord
+        {
+            FirstName = "Anna", LastName = "Berlin", MeetingTime = DateTimeOffset.Now.AddHours(1),
+        },
+        new GuestRecord
+        {
+            FirstName = "Dimitrios", LastName = "Papadopoulos", MeetingTime = DateTimeOffset.Now.AddHours(2),
+        },
+        new GuestRecord
+        {
+            FirstName = "Maria", LastName = "Khan", MeetingTime = DateTimeOffset.Now.AddHours(3),
+        }
+    };
 }
--- OlympusVMS/OlympusVMS/Program.cs
+++ OlympusVMS/OlympusVMS/Program.cs
@@ -1,6 +1,3 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using OlympusVMS.Components;
 
 var builder = WebApplication.CreateBuilder(args);
