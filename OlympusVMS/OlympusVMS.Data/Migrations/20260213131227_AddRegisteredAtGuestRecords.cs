cd10c8c8d0d85f1fa6f4f85787454bc6e2bb0e5a
jimtete <48908714+jimtete@users.noreply.github.com>
2026-02-13 14:45:22 +0100

    Field Added

---
+++ OlympusVMS/OlympusVMS.Data/Migrations/20260213131227_AddRegisteredAtGuestRecords.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OlympusVMS.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRegisteredAtGuestRecords : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "RegisteredAt",
+                schema: "Olympus",
+                table: "GuestRecord",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValueSql: "SYSDATETIMEOFFSET()");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RegisteredAt",
+                schema: "Olympus",
+                table: "GuestRecord");
+        }
+    }
+}
